64985edc71593ddd7813b26b7d8406a0bb35de79
raullc92 <89393783+raullc92@users.noreply.github.com>
2022-04-05 19:42:57 +0200

    Install and configure DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#3ABFF8",
+          secondary: "#6419E6",
+          accent: "#D926A9",
+          neutral: "#1D283A",
+          "base-100": "#191D24",
+          info: "#0CA6E9",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#FB6F84",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
